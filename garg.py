98fbb2f98c08c5976d78f1c10bb8113d30493081
ssmythe <steve@smythefamily.com>
2023-04-27 14:26:22 -0700

    initial commit

---
+++ garg.py
@@ -0,0 +1,5 @@
+def hello(person):
+    print(f"Hello {person}!")
+
+def goodbye(person):
+    print(f"Goodbye {person}!")
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='garg',
+    version='0.1',
+    description='Python package example',
+    author='Your Name',
+    url='https://github.com/ssmythe/garg',
+    license='Apache 2.0',
+    py_modules=['garg']
+)
